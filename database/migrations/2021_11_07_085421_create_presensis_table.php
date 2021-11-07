7399faeff29a3079a47184c040e75f08ffb5765f
MuHajir <hajir.nf@gmail.com>
2021-11-07 15:57:54 +0700

    implement presensi

---
+++ database/migrations/2021_11_07_085421_create_presensis_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePresensisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presensis', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id');
+            $table->string('topic');
+            $table->text('question')->nullable();
+            $table->dateTime('open_date');
+            $table->dateTime('close_date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presensis');
+    }
+}
