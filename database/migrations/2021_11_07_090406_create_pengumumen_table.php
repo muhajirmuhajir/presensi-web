df2c9371f7895c2d4207f2688b78043ac528ebfb
MuHajir <hajir.nf@gmail.com>
2021-11-07 16:08:22 +0700

    implement pengumuman

---
+++ database/migrations/2021_11_07_090406_create_pengumumen_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengumumenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengumumans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('kelas_id')->nullable();
+            $table->string('title');
+            $table->longText('body');
+            $table->text('thumbnail_url');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengumumen');
+    }
+}
