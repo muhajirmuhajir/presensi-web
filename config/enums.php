8aa503420bdbc6babfd601ba5821080ee305da9a
MuHajir <hajir.nf@gmail.com>
2021-11-13 11:54:10 +0700

    add laravel permission

---
+++ config/enums.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'permissions' => [
+        'create_presensi_record',
+        'create_presensi',
+        'create_pengumuman',
+        'create_kelas',
+        'create_course',
+        'create_user'
+    ],
+    'roles' => [
+        'student' => 'student',
+        'teacher' => 'teacher',
+        'bk' => 'bk',
+        'super_admin' => 'super_admin',
+    ],
+];
