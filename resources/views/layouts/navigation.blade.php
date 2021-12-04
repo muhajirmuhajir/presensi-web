1ad72978f009c422b7a74a3d758d43ba8e8937b5
MuHajir <hajir.nf@gmail.com>
2021-12-04 15:56:42 +0800

    hot fix: link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -122,9 +122,33 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('presensi.index')" :active="request()->routeIs('presensi.*')">
+                {{ __('Presensi') }}
             </x-responsive-nav-link>
+            @role(config('enums.roles.teacher'))
+            <x-responsive-nav-link :href="route('course.index')" :active="request()->routeIs('course.*')">
+                {{ __('Kelas Saya') }}
+            </x-responsive-nav-link>
+            @endrole
+            <x-responsive-nav-link :href="route('pengumuman.index')" :active="request()->routeIs('pengumuman.*')">
+                {{ __('Pengumuman') }}
+            </x-responsive-nav-link>
+            @role(config('enums.roles.bk'))
+
+            <x-responsive-nav-link :href="route('kelas.index')" :active="request()->routeIs('kelas.*')">
+                {{ __('Kelas') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('course.index')" :active="request()->routeIs('course.*')">
+                {{ __('Mata Pelajaran') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('student.index')" :active="request()->routeIs('student.*')">
+                {{ __('Siswa') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('teacher.index')" :active="request()->routeIs('teacher.*')">
+                {{ __('Guru') }}
+            </x-responsive-nav-link>
+            @endrole
         </div>
 
         <!-- Responsive Settings Options -->
